73be5af54913ebc4179c2e72d2d6a968479ff5e1
rasel726 <md.rasel2726@gmail.com>
2022-08-23 01:28:53 +0600

    27-6 (advanced) Share one function between two input field

--- functional-baap-er-bank/js/deposit.js
+++ functional-baap-er-bank/js/deposit.js
@@ -1,7 +1,24 @@
+function getInputValueById(inputId){
+    const inputField = document.getElementById(inputId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+function getElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value; 
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function(){
     /*
     1. get the element by id
     2. get the value from the element
     3. convert value to a number
     */
+
+    const newDepositAmount = getInputValueById('deposit.field');
 })

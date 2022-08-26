076ac49a6118081e3bef6ceca6aaae1e5cd741eb
rasel726 <md.rasel2726@gmail.com>
2022-08-26 23:58:44 +0600

    27-7 (advanced) get and set element value using a function

--- functional-baap-er-bank/js/deposit.js
+++ functional-baap-er-bank/js/deposit.js
@@ -1,24 +1,24 @@
-function getInputValueById(inputId){
-    const inputField = document.getElementById(inputId);
-    const inputFieldValueString = inputField.value;
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    inputField.value = '';
-    return inputFieldValue;
-}
+document.getElementById('btn-deposit').addEventListener('click', function () {
+    /**
+     1. get the element by ID
+     2. get the value from the element 
+     3. convert string value to a number
+     */
 
-function getElementValueById(elementId){
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const value = parseFloat(elementValueString);
-    return value; 
-}
+    const newDepositAmount = getInputFieldValueById('deposit-field');
+    /**
+     1. get previous deposit total by ID 
+     */
+    const previousDepositTotal = getTextElementValueById('deposit-total');
 
-document.getElementById('btn-deposit').addEventListener('click', function(){
-    /*
-    1. get the element by id
-    2. get the value from the element
-    3. convert value to a number
-    */
+    // calculate new deposit total
+    const newDepositTotal = previousDepositTotal + newDepositAmount;
+    //  set deposit total value 
+    setTextElementValueById('deposit-total', newDepositTotal);
 
-    const newDepositAmount = getInputValueById('deposit.field');
+    // get previous balance by using the function
+    const previousBalanceTotal = getTextElementValueById('balance-total');
+    const newBalanceTotal = previousBalanceTotal + newDepositAmount;
+    setTextElementValueById('balance-total', newBalanceTotal);
+
 })
